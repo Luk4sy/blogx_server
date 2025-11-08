80e553907644fa96a09e4e96cde7a0e2db4523d7
Luk4sy <302597565@qq.com>
2025-11-08 16:56:07 +0800

    feat-配置文件读取

---
+++ core/init_conf.go
@@ -0,0 +1,31 @@
+package core
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"os"
+)
+
+var confPath = "setting.yaml"
+
+type System struct {
+	IP   string `yaml:"ip"`
+	Port int    `yaml:"port"`
+}
+
+type Config struct {
+	System System `yaml:"system"`
+}
+
+func ReadConf() {
+	byteData, err := os.ReadFile(confPath)
+	if err != nil {
+		panic(err)
+	}
+	var config Config
+	err = yaml.Unmarshal(byteData, &config)
+	if err != nil {
+		panic(fmt.Sprintf("yaml 配置文件格式错误 %s", err))
+	}
+	fmt.Println(config)
+}
